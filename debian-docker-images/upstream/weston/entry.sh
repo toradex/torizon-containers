b02b958e8f0fd3225963d0a34fa5974fbec7bd73
Leonardo Held <leonardo.held@toradex.com>
2023-09-27 15:21:20 -0300

    Remove iMX-specific code
    Signed-off-by: Leonardo Held <leonardo.held@toradex.com>

--- debian-docker-images/upstream/weston/entry.sh
+++ debian-docker-images/upstream/weston/entry.sh
@@ -16,10 +16,7 @@ declare -a WESTON_EXTRA_ARGS
 OPTIONS=developer,no-change-tty,tty:
 
 WAYLAND_USER=${WAYLAND_USER:-torizon}
-WESTON_ARGS=${WESTON_ARGS:--Bdrm-backend.so --current-mode -S${WAYLAND_DISPLAY}}
-ENABLE_VNC=${ENABLE_VNC:-0}
-ENABLE_RDP=${ENABLE_RDP:-0}
-IGNORE_X_LOCKS=${IGNORE_X_LOCKS:-0}
+WESTON_ARGS=${WESTON_ARGS:---backend=drm-backend.so --tty 7}
 IGNORE_VT_SWITCH_BACK=${IGNORE_VT_SWITCH_BACK:-0}
 
 #
@@ -98,44 +95,6 @@ fi
 set -- "${WESTON_EXTRA_ARGS[@]}"
 
 #
-# Detect the SoC and supported features.
-#
-
-SOC_ID=''
-SOC_ID_FILE='/sys/devices/soc0/soc_id'
-test -e $SOC_ID_FILE && SOC_ID=$(<$SOC_ID_FILE)
-if test -n "$SOC_ID"; then
-	echo "SoC is: '$SOC_ID'"
-else
-	echo "Cannot detect SoC! Assuming it's GPU-capable."
-fi
-HAS_GPU=true
-HAS_DPU=false
-
-function has_feature() {
-	FEATURE=$1
-	PATTERNS_FILE=/etc/imx_features/${FEATURE}.socs
-	ANSWER=false
-	test -r "$PATTERNS_FILE" && grep -qf "$PATTERNS_FILE" <<<"$SOC_ID" && ANSWER=true
-	echo $ANSWER
-}
-
-test -n "$SOC_ID" && {
-	HAS_GPU=$(has_feature 'imxgpu')
-	HAS_DPU=$(has_feature 'imxdpu')
-}
-echo "SoC has GPU: $HAS_GPU"
-echo "SoC has DPU: $HAS_DPU"
-
-#
-# Decide on what g2d implementation must be enabled for weston.
-#
-
-G2D_IMPLEMENTATION='viv'
-$HAS_DPU && G2D_IMPLEMENTATION='dpu'
-echo "g2d implementation: $G2D_IMPLEMENTATION"
-test -e /etc/alternatives/libg2d.so.1.5 && update-alternatives --set libg2d.so.1.5 /usr/lib/aarch64-linux-gnu/libg2d-${G2D_IMPLEMENTATION}.so
-
 #
 # Set desktop defaults.
 #
@@ -166,9 +125,16 @@ function init_xdg() {
 
 init_xdg
 
-#
-# Execute the weston compositor.
-#
+function cleanup() {
+	if [ "$IGNORE_VT_SWITCH_BACK" != "1" ]; then
+		# switch back to tty1, otherwise the console screen is not displayed.
+		echo "Switching back to vt ${OLD_VT:3}"
+		chvt "${OLD_VT:3}"
+	fi
+        rm -rf /run/seatd.sock
+}
+
+trap cleanup EXIT
 
 function init() {
 	if CMD=$(command -v "$1" 2>/dev/null); then
@@ -181,72 +147,11 @@ function init() {
 	fi
 }
 
-$HAS_GPU || $HAS_DPU || {
-	echo "Fallbacking to software renderer."
-	WESTON_ARGS="${WESTON_ARGS} --use-pixman"
-}
-
-REMOTE_UI="[screen-share]"
-VNC_BACKEND="command=/usr/bin/weston --backend=vnc-backend.so --shell=fullscreen-shell.so"
-RDP_BACKEND="command=/usr/bin/weston --backend=rdp-backend.so --shell=fullscreen-shell.so --no-clients-resize  --rdp-tls-key=/var/volatile/tls.key --rdp-tls-cert=/var/volatile/tls.crt --force-no-compression"
-START_ON_STARTUP_CONFIG="start-on-startup=true"
-CONFIGURATION_FILE=/etc/xdg/weston/weston.ini
-CONFIGURATION_FILE_DEV=/etc/xdg/weston-dev/weston.ini
-
-if [ "$ENABLE_VNC" = "1" ]; then
-	MSG=$REMOTE_UI"\n$VNC_BACKEND\n"$START_ON_STARTUP_CONFIG
-	echo -e "$MSG" | tee -a $CONFIGURATION_FILE $CONFIGURATION_FILE_DEV 1>/dev/null
-fi
-
-if [ "$ENABLE_RDP" = "1" ]; then
-	mkdir -p /var/volatile
-	{
-		MSG=$REMOTE_UI"\n$RDP_BACKEND\n"$START_ON_STARTUP_CONFIG
-		echo -e "$MSG" | tee -a $CONFIGURATION_FILE $CONFIGURATION_FILE_DEV 1>/dev/null
-
-		if [ ! -f /var/volatile/tls.crt ] || [ ! -f /var/volatile/tls.key ]; then
-			echo "Certificates for RDP not found in /var/volatile"
-			cd /var/volatile || exit
-			openssl genrsa -out tls.key 2048 &&
-				openssl req -new -key tls.key -out tls.csr \
-					-subj "/C=CH/ST=Luzern/L=Luzern/O=Toradex/CN=www.toradex.com" &&
-				openssl x509 -req -days 365 -signkey tls.key \
-					-in tls.csr -out tls.crt
-			chmod 0644 tls.key tls.crt
-			if [ -f "tls.crt" ]; then
-				echo "Certificate for RDP successfully generated"
-			else
-				echo "Error generating certificate for RDP"
-			fi
-			cd || exit
-		else
-			echo "Certificates for RDP found in /var/volatile. Skipping generation."
-		fi
-	} 2>&1 | tee -a /var/volatile/weston.log
-fi
-
-if [ "$IGNORE_X_LOCKS" != "1" ]; then
-	echo "Removing previously created '.X*-lock' entries under /tmp before starting Weston. Pass 'IGNORE_X_LOCKS=1' environment variable to Weston container to disable this behavior."
-	rm -rf /tmp/.X*-lock
-fi
-
-function cleanup() {
-	if [ "$IGNORE_VT_SWITCH_BACK" != "1" ]; then
-		# switch back to tty1, otherwise the console screen is not displayed.
-		echo "Switching back to vt ${OLD_VT:3}"
-		chvt "${OLD_VT:3}"
-	fi
-        rm -rf /run/seatd.sock
-}
-
-trap cleanup EXIT
-
-dos2unix $CONFIGURATION_FILE
-dos2unix $CONFIGURATION_FILE_DEV
-
 # for every argument after "--", append that argument to WESTON_ARGS
 for i in "${!WESTON_EXTRA_ARGS[@]}"; do
 	WESTON_ARGS+=" "${WESTON_EXTRA_ARGS[$i]}
 done
 
-rm -rf /run/seatd.sock || true && init seatd-launch -- weston "${WESTON_ARGS}"
+# FIXME: this should be run as ... init seatd-launch ... to make sure Weston
+# is running as 'torizon' user, but it didn't really work.
+rm -rf /run/seatd.sock || true && seatd-launch -- weston ${WESTON_ARGS}
